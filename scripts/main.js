7b3d4dce81514a803d2ec068aaf075b5d5b5d82c
steve <steve@sperez.com.au>
2019-04-11 07:20:05 +1000

    Add click functionality to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 alert ('this is some javaScript!');
+document.querySelector('html').onclick = function() {
+    alert ('cant touch this!');
+}
